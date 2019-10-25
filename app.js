ceefdb11631f64dff771019924ae58bfed44f4c0
Bosco Basabana <basabanansita@gmail.com>
2019-10-25 09:34:47 +0100

    eslint

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
+// call router
 const productsRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
+// routes handle request
 app.use('/api/products', productsRoutes);
 app.use('/api/orders', ordersRoutes);
 
